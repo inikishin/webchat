4a02cd92248f60fbd77ddb95a0d6ef1ed092d82c
darkstorn <inikishin@gmail.com>
2021-09-21 17:15:58 +0300

    add chat logging

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,30 +1,38 @@
 import json
+import logging
 from channels.generic.websocket import WebsocketConsumer
 from asgiref.sync import async_to_sync
 
+logger = logging.getLogger(__name__)
+
 class ChatConsumer(WebsocketConsumer):
-    async def connect(self):
-        self.room_name = self.scope['url_route']['kwargs']['room_name']
-        self.room_group_name = 'chat'
+    def connect(self):
+        chat_name = self.scope['url_route']['kwargs']['room_name']
+        logger.warning(f'connection with chat "{chat_name}" established')
+        self.room_name = chat_name
+        self.room_group_name = chat_name
 
-        await async_to_sync(self.channel_layer.group_add)(
+        async_to_sync(self.channel_layer.group_add)(
             self.room_group_name,
             self.channel_name
         )
 
-        await self.accept()
+        self.accept()
 
-    async def disconnect(self, code):
-        await async_to_sync(self.channel_layer.group_discard)(
+    def disconnect(self, code):
+        logger.warning('connection closed')
+        async_to_sync(self.channel_layer.group_discard)(
             self.room_group_name,
             self.channel_name
         )
 
-    async def receive(self, text_data=None, bytes_data=None):
+    def receive(self, text_data=None, bytes_data=None):
+        logger.warning(f'message recived: {text_data}')
         text_data_json = json.loads(text_data)
         message = text_data_json['message']
+        print(text_data)
 
-        await async_to_sync(self.channel_layer.group_send)(
+        async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
                 'type': 'chat_message',
@@ -32,9 +40,10 @@ class ChatConsumer(WebsocketConsumer):
             }
         )
 
-    async def chat_message(self, event):
+    def chat_message(self, event):
         message = event['message']
+        print('123', event)
 
-        await self.send(text_data=json.dumps({
+        self.send(text_data=json.dumps({
             'message': message
         }))
